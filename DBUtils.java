7e79f8e391930d49bb969e6a5c77e92ff8a1738f
srinivasrathode <srathode428@gmail.com>
2022-12-31 16:14:04 +0530

    added new files

--- DBUtils.java
+++ DBUtils.java
@@ -1,2 +1,3 @@
 This is the  Database Mnagement platform consists of some DBlogic
 Hello im updating
+git reset command is usee to revert files from staging area to working area
---
+++ test.py
@@ -0,0 +1,1 @@
+Hello this filen consists of pyhthon programe
